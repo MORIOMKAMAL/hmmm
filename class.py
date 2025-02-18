eb3228936090c5c7833b0904e21a5a486b935a2e
MORIOMKAMAL <moriomkamal7@gmail.com>
2025-02-18 11:58:12 +0600

    class

---
+++ class.py
@@ -0,0 +1,10 @@
+class hira:
+    def __int__(self, name , age):
+        self.name = name
+        self.age = age
+    def show(self):
+        print(f"Name : {self.name} and Age : {self.age}")
+
+
+object1 = hira()
+object1.show()
